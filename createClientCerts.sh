d44fc443bce51df4960e3237e5eff71d7bdd13b4
paolone <paolo.morgano@gmail.com>
2019-09-24 19:50:55 +0200

    create ca, admin and client certs

---
+++ createClientCerts.sh
@@ -0,0 +1,35 @@
+INSTANCES=( node1 node2 )
+INSTANCES_IP=( 10.211.55.61 10.211.55.62) 
+for i in 0 1 
+do
+cat > ${INSTANCES[$i]}-csr.json <<EOF
+{
+  "CN": "system:node:node$${INSTANCES[$i]}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "IT",
+      "L": "Lecco",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Lombardia"
+    }
+  ]
+}
+EOF
+
+EXTERNAL_IP=${INSTANCES_IP[$i]}
+
+INTERNAL_IP=${INSTANCES_IP[$i]}
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=${INSTANCES[$i]},${INTERNAL_IP} \
+  -profile=kubernetes \
+  ${INSTANCES[$i]}-csr.json | cfssljson -bare ${INSTANCES[$i]}
+done
