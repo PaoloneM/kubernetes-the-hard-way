ad6f92ef431f71ed37a627dec6046bdf5dbd5588
Paolo Morgano <paolo.morgano@gmail.com>
2019-10-10 22:02:38 +0200

    fix missing ca key on controllers

--- 04-certificate-authority/09-copyCerts.sh
+++ 04-certificate-authority/09-copyCerts.sh
@@ -7,6 +7,7 @@ done
 
 for instance in controller-0 controller-1; do
     vagrant scp ../04-certificate-authority/ca.pem ${instance}:~/ 
+    vagrant scp ../04-certificate-authority/ca-key.pem ${instance}:~/ 
     vagrant scp ../04-certificate-authority/kubernetes-key.pem ${instance}:~/
     vagrant scp ../04-certificate-authority/kubernetes.pem ${instance}:~/
     vagrant scp ../04-certificate-authority/service-account-key.pem ${instance}:~/
